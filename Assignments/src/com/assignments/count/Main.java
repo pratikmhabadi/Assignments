b2412ba8c33157175cc6ea5a60f93e6d1d0789e6
Pratik <pratik.mhabadi@smarteinc.com>
2021-10-08 15:50:51 +0530

    added String count

---
+++ Assignments/src/com/assignments/count/Main.java
@@ -0,0 +1,33 @@
+package com.assignments.count;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Your Input :");
+        String str = sc.next().toLowerCase(Locale.ROOT);
+
+        char[] string = str.toCharArray();
+        int [] count = new int[str.length()];
+
+        for(int i = 0; i <str.length(); i++) {
+            count[i] = 1;
+            for(int j = i+1; j <str.length(); j++) {
+                if(string[i] == string[j]) {
+                    count[i]++;
+                   string[j] = '0';
+                }
+            }
+        }
+
+
+        System.out.println("Your Output");
+        for(int i = 0; i <count.length; i++) {
+            if(string[i] != ' ' && string[i] != '0')
+                System.out.println(string[i] + "-" + count[i]);
+        }
+
+    }
+}
