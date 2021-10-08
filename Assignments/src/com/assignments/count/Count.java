fcf18c76d63f4dd5b47f10be4635aabd4c0efa0c
Pratik <pratik.mhabadi@smarteinc.com>
2021-10-08 17:44:37 +0530

    add String count using collection

---
+++ Assignments/src/com/assignments/count/Count.java
@@ -0,0 +1,25 @@
+package com.assignments.count;
+
+import java.util.*;
+
+public class Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next().toLowerCase(Locale.ROOT);
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : str.toCharArray()) {
+            if (map.get(c) != null) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> mapmap : map.entrySet()) {
+            System.out.println(mapmap.getKey() + "=" + mapmap.getValue());
+        }
+
+
+    }
+}
--- Assignments/src/com/assignments/count/Main.java
+++ Assignments/src/com/assignments/count/Main.java
@@ -5,27 +5,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your Input :");
         String str = sc.next().toLowerCase(Locale.ROOT);
 
         char[] string = str.toCharArray();
-        int [] count = new int[str.length()];
+        int[] count = new int[str.length()];
 
-        for(int i = 0; i <str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) {
             count[i] = 1;
-            for(int j = i+1; j <str.length(); j++) {
-                if(string[i] == string[j]) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (string[i] == string[j]) {
                     count[i]++;
-                   string[j] = '0';
+                    string[j] = '0';
                 }
             }
         }
 
 
         System.out.println("Your Output");
-        for(int i = 0; i <count.length; i++) {
-            if(string[i] != ' ' && string[i] != '0')
+        for (int i = 0; i < count.length; i++) {
+            if (string[i] != ' ' && string[i] != '0')
                 System.out.println(string[i] + "-" + count[i]);
         }
 
